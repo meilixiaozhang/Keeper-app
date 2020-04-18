0df98142ae9e3080c2e09e29b8450550571de19c
meilixiaozhang <redpangci@gmail.com>
2020-04-18 23:12:51 +1000

    render custom notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
 import Note from "./components/Note";
+import notes from "./notes";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
