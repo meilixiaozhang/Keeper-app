40f84ebf353d8b67b8caaaab992ab07d311ac489
meilixiaozhang <redpangci@gmail.com>
2020-04-23 19:01:25 +1000

    update css with material-ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
-  const [inputNote, setInputNote] = useState({
+  const [clicked, setClicked] = useState(false);
+
+  const [note, setNote] = useState({
     title: "",
     content: "",
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setInputNote((prevNote) => {
+
+    setNote((prevNote) => {
       return {
         ...prevNote,
         [name]: value,
@@ -16,34 +22,45 @@ function CreateArea(props) {
     });
   }
 
+  function handleClick() {
+    setClicked(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          onChange={handleChange}
-          name="title"
-          placeholder="Title"
-          value={inputNote.title}
-        />
+      <form className="create-note">
+        {clicked && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
-          onChange={handleChange}
+          onClick={handleClick}
           name="content"
+          onChange={handleChange}
+          value={note.content}
           placeholder="Take a note..."
-          rows="3"
-          value={inputNote.content}
+          rows={clicked ? "3" : "1"}
         />
-        <button
-          onClick={(event) => {
-            props.onAdd(inputNote);
-            setInputNote({
-              title: "",
-              content: "",
-            });
-            event.preventDefault();
-          }}
-        >
-          Add
-        </button>
+        {clicked && (
+          <Zoom in={true}>
+            <Fab
+              onClick={(event) => {
+                props.onAdd(note);
+                setNote({
+                  title: "",
+                  content: "",
+                });
+                event.preventDefault();
+              }}
+            >
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        )}
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
+  function handleClick() {
+    props.onChecked(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button
-        onClick={() => {
-          props.onChecked(props.id);
-        }}
-      >
-        DELETE
+      <button onClick={handleClick}>
+        <DeleteIcon />
       </button>
     </div>
   );
